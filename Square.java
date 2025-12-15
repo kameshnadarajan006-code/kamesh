6a349a5f28c30e40dbf30f91d027e916487822fc
Kamesh <kameshnadarajan006@gmail.com>
2025-12-15 07:38:12 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (l == b)
+            System.out.println("Square");
+        else
+            System.out.println("Rectangle");
+    }
+}
